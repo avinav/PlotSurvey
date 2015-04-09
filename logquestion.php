75aa4615bf73438f57cd0a9cc28bb9416dfa4f91
avinav <sharan.avinav@gmail.com>
2015-04-09 03:50:10 -0400

    generate data from db

--- logquestion.php
+++ logquestion.php
@@ -1,102 +1,12 @@
-<?php
-/**
- * set up database connection
- * @param unknown $dbhost
- * @param unknown $dbuser
- * @param unknown $dbpass
- * @return resource
- */
-function db_conn($dbhost, $dbuser, $dbpass) {
-	$conn = mysql_connect ( $dbhost, $dbuser, $dbpass );
-	if (! $conn) {
-		die ( 'Could not connect: ' + mysql_error () );
-	}
-	echo '\nConnected successfully';
-	return $conn;
-}
-/**
- * Assume the max id in questions table to be latest entry, 
- * Use maxid to set foreign key in answers table
- * @param unknown $conn
- */
-function selectMaxId($conn) {
-	$sql = 'SELECT MAX(qid) AS `maxid` FROM `questions`';
-	$retval = mysql_query( $sql, $conn );
-	if(! $retval )
-	{
-		die('Could not select max ' . mysql_error());
-	}
-	$row = mysql_fetch_array($retval, MYSQL_NUM);
-	echo 'MAXID:'.$row[0];
-	return $row[0];
-}
-/**
- * insert the data (question and answer) in corresponding tables
- * @param unknown $dbname
- * @param unknown $qtext
- * @param unknown $atext
- * @param unknown $conn
- */
-function insertquestion($dbname, $qtext, $atextlist, $conn) {
-	mysql_select_db($dbname);
-	
-	// Insert into questions table
-	$sql = "INSERT INTO questions ".
-			"(qtext) ".
-			"VALUES ".
-			"('$qtext')";
-	$retval = mysql_query( $sql, $conn );
-	if(! $retval )
-	{
-		die('Could not enter data: ' . mysql_error());
-	}
-
-	// Get question id to set the foreign key of answers table
-	$qid = selectMaxId($conn);
-	
-	// Insert into answers table
-	foreach($atextlist as $atext) {
-		$sql = "INSERT INTO answers ".
-				"(atext,quesid) ".
-				"VALUES ".
-				"('$atext',$qid)";
-		$retval = mysql_query( $sql, $conn );
-		if(! $retval )
-		{
-			die('Could not enter data: ' . mysql_error());
-		}
-	}
-}
-//----------------main-------------------
-// on POST request
-if (isset($_POST['insertq'])) {
-	$dbhost = 'localhost:3036';
-	$dbuser = 'root';
-	$dbpass = 'oxford';
-	$dbname = 'CSE574';
-	
-	// Get data from the input text fields
-	$conn = db_conn($dbhost, $dbuser, $dbpass);
-	$qtext = $_POST['inp_ques'];
-	$atext = $_POST["inp_ans"];
-	
-	// split data on ','
-	$atextlist = explode(",",$atext);
-	
-	// Insert in database
-	insertquestion($dbname, $qtext,$atextlist,$conn);
-	mysql_close($conn);
-}
-
-?>
 <html>
 <head>
 <title>Insert questions</title>
 </head>
 <body>
+	<h1>Welcome!</h1>
 	<div id='qform'
-		<?php if (isset($_POST['insertq'])){ echo ' style="display:none;"'; } ?>>
-		<form name="question_form" action="<?php $_PHP_SELF ?>" method="POST">
+		<?php //if (isset($_POST['insertq'])){ echo ' style="display:none;"'; } ?>>
+		<form name="question_form" action="question.php" method="POST">
 			<table>
 				<tr>
 					<td><span>Enter Question:</span></td>
@@ -113,6 +23,6 @@ if (isset($_POST['insertq'])) {
 			</table>
 		</form>
 	</div>
-	<?php if(isset($_POST['insertq'])) { echo "<div id='qres'>Insert successfull!</div>"; }?>
+	<?php // if(isset($_POST['insertq'])) { echo "<div id='qres'>Insert successfull!</div>"; }?>
 	</body>
 </html>
--- question.php
+++ question.php
@@ -1,47 +1,140 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-<link type="text/css" rel="stylesheet" href="css/stylesheet.css" />
-<title>Poll</title>
+<title>Question</title>
 </head>
-<body>
-<div id = 'header'>
-<h1>Distribution Generator</h1>
-</div>
-<?php 
-session_start();
+<?php
+/**
+ * set up database connection
+ *
+ * @param unknown $dbhost
+ * @param unknown $dbuser
+ * @param unknown $dbpass
+ * @return resource
+ */
+function db_conn($dbhost, $dbuser, $dbpass) {
+	$conn = mysql_connect ( $dbhost, $dbuser, $dbpass );
+	if (! $conn) {
+		die ( 'Could not connect: ' + mysql_error () );
+	}
+	return $conn;
+}
 
-$_SESSION['pageviews'] = ($_SESSION['pageviews']) ? $_SESSION['pageviews'] + 1 : 1;
-$qText = "Select any of the numbers:" + $_SESSION['pageviews']?>
+
+/**
+ * Assume the max id in questions table to be latest entry,
+ * Use maxid to set foreign key in answers table
+ *
+ * @param unknown $conn
+ */
+function selectMaxId($conn) {
+	$sql = 'SELECT MAX(qid) AS `maxid` FROM `questions`';
+	$retval = mysql_query ( $sql, $conn );
+	if (! $retval) {
+		die ( 'Could not select max ' . mysql_error () );
+	}
+	$row = mysql_fetch_array ( $retval, MYSQL_NUM );
+	return $row [0];
+}
+
+
+/**
+ * insert the data (question and answer) in corresponding tables
+ *
+ * @param unknown $dbname
+ * @param unknown $qtext
+ * @param unknown $atext
+ * @param unknown $conn
+ */
+function insertquestion($dbname, $qtext, $atextlist, $conn) {
+	mysql_select_db ( $dbname );
+
+	// Insert into questions table
+	$sql = "INSERT INTO questions " . "(qtext) " . "VALUES " . "('$qtext')";
+	$retval = mysql_query ( $sql, $conn );
+	if (! $retval) {
+		die ( 'Could not enter data: ' . mysql_error () );
+	}
+
+	// Get question id to set the foreign key of answers table
+	$qid = selectMaxId ( $conn );
+
+	// Insert into answers table
+	foreach ( $atextlist as $atext ) {
+		$sql = "INSERT INTO answers " . "(atext,quesid) " . "VALUES " . "('$atext',$qid)";
+		$retval = mysql_query ( $sql, $conn );
+		if (! $retval) {
+			die ( 'Could not enter data: ' . mysql_error () );
+		}
+	}
+	return $qid;
+}
+// ----------------main-------------------
+$qtext = "";
+$qid = "";
+$atextlist = array();
+// on POST request
+if (isset ( $_POST ['insertq'] )) {
+	$dbhost = 'localhost:3036';
+	$dbuser = 'root';
+	$dbpass = 'oxford';
+	$dbname = 'CSE574';
+	
+	// Get data from the input text fields
+//	$conn = db_conn ( $dbhost, $dbuser, $dbpass );
+	$qtext = $_POST ['inp_ques'];
+	$atext = $_POST ["inp_ans"];
+	
+	// split data on ','
+	$atextlist = explode ( ",", $atext );
+	
+	// Insert in database
+//	$qid = insertquestion ( $dbname, $qtext, $atextlist, $conn );
+	
+	mysql_close ( $conn );
+}
+
+?>
+	<body>
+	<div id='header'>
+		<h1>Poll Survey</h1>
+	</div>
+
 	<div id="question">
-	<form name ="question_form" action ="graph.php" method = "POST" >
+		<form name="question_form" action="graph.php" method="POST">
 			<table class="question_tb">
 				<tr>
 					<td><span>Question </span></td>
-					<td><div id = 'question_text'><?php if(isset($qText)){ echo $qText; } ?></div></td>
-				
-				<tr>
-					<td><input type="radio" name = "ans" value = "ans1">1</input></td>
+					<td><div id='question_text'><?php if(isset($qText)){ echo $qText; } ?></div></td>
+				</tr>
+				<?php
+				$i = 0; 
+				foreach ($atextlist as $atext) {
+					echo "<tr><td><input type='radio' name='$qid' value='$i'>$atext</input></td></tr>";
+				}
+				?>
+				<!-- 
+				<tr>				
+					<td><input type="radio" name="ans" value="ans1">1</input></td>
 				</tr>
 				<tr>
-					<td><input type="radio" name = "ans" value = "ans2"/>2</td>
+					<td><input type="radio" name="ans" value="ans2" />2</td>
 				</tr>
 				<tr>
-					<td><input type="radio" name = "ans" value = "ans3"/>3</td>
+					<td><input type="radio" name="ans" value="ans3" />3</td>
 				</tr>
 				<tr>
-					<td><input type="radio" name = "ans" value = "ans4"/>4</td>
+					<td><input type="radio" name="ans" value="ans4" />4</td>
 				</tr>
+				-->
 				<tr>
-					<td colspan="2" align="right"><input type="button"
-						value="submit" onclick="submitResponse();" /></td>
+					<td colspan="2" align="right"><input type="button" value="submit"
+						onclick="submitResponse();" /></td>
 				</tr>
 			</table>
 		</form>
-</div>
-<div id="footer">
-
-</div>
+	</div>
+	<div id="footer"></div>
 </body>
 </html>
 
